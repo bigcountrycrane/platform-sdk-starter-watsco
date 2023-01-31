05fa5fa9034b54116589d7d075f4c17ff3742dd8
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-01-31 09:50:01 -0700

    Updates for SmartDate Field
    - Updates for default
    - Need to add compute / validate for number fields + padding

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import {
-//  BooleanField,
-  DateField,
   Message,
   NumberField,
   OptionField,
-//  Portal,
   Sheet,
   TextField,
   Workbook,
+  SpaceConfig,
 } from '@flatfile/configure'
+import { SmartDateField } from './SmartDateField'
 
 // import { FlatfileRecord, FlatfileRecords } from '@flatfile/hooks'
 // import fetch from 'node-fetch'
@@ -17,15 +16,17 @@ import crono from 'chrono-node'
 const Branch_Supplemental = new Sheet(
   'Branch Supplemental',
   {
+    //must be 3 digits, numeric only, padded with leading zeros
+
     branch_code: NumberField({
-      label: "Branch Code",
+      label: 'Branch Code',
       required: true,
       unique: true,
       description: 'Code of the branch',
     }),
     branch_name: TextField({
-      label: "Branch Name",
-      required: true
+      label: 'Branch Name',
+      required: true,
     }),
     branch_type: OptionField({
       label: 'Branch Type',
@@ -35,78 +36,97 @@ const Branch_Supplemental = new Sheet(
         field_operations: { label: 'Field Operations' },
         sales_center: { label: 'Sales Center' },
         hub: { label: 'Hub' },
-        distribution_center: { label: 'Distribution Center' }
+        distribution_center: { label: 'Distribution Center' },
       },
     }),
     alt_region_code: TextField({
-      label: "Alt Region Code",
-      validate: (alt_region_code: string) => {
-      const alphaRegex = /^[a-z0-9]+$/i
-      if (!alphaRegex.test(alt_region_code)) {
-        return [
-          new Message(
-            `${alt_region_code} should be alphanumeric`,
-            'error',
-            'validate'
-          ),
-        ]
-      }
-    },
+      label: 'Alt Region Code',
     }),
     alt_region_name: TextField({
-      label: "Alt Region Name",
-      validate: (alt_region_name: string) => {
-        const alphaRegex = /^[a-z0-9]+$/i
-        if (!alphaRegex.test(alt_region_name)) {
-          return [
-            new Message(
-              `${alt_region_name} should be alphanumeric`,
-              'error',
-              'validate'
-            ),
-          ]
-        }
-      },
+      label: 'Alt Region Name',
     }),
+
+    //must be 3 digits, numeric only, padded with leading zeros
     branch_group_code: NumberField({
-      label: "Branch Group Code"
+      label: 'Branch Group Code',
     }),
     branch_group_name: TextField({
-      label: "Branch Group Name"
+      label: 'Branch Group Name',
     }),
-    open_date: DateField({
-      label: "Open Date",
-      required: true
+    open_date: SmartDateField({
+      label: 'Open Date',
+      formatString: 'yyyy-MM-dd',
+      required: true,
     }),
-    close_date: DateField({
-      label: "Close Date"
+
+    //ifempty = 1900-01-01
+    close_date: SmartDateField({
+      label: 'Close Date',
+      formatString: 'yyyy-MM-dd',
+      default: new Date('1900-01-01'),
+      annotations: {
+        default: true,
+        defaultMessage: 'Effective date has been formatted and defaulted to:',
+      },
     }),
-    active_date: DateField({
-      label: "Active Date"
+
+    //ifempty = 1900-01-01
+    active_date: SmartDateField({
+      label: 'Active Date',
+      formatString: 'yyyy-MM-dd',
+      default: new Date('1900-01-01'),
+      annotations: {
+        default: true,
+        defaultMessage: 'Effective date has been formatted and defaulted to:',
+      },
     }),
-    last_relo_date: DateField({
-      label: "Last Relo Date"
+
+    //ifempty = 1900-01-01
+    last_relo_date: SmartDateField({
+      label: 'Last Relo Date',
+      formatString: 'yyyy-MM-dd',
+      default: new Date('1900-01-01'),
+      annotations: {
+        default: true,
+        defaultMessage: 'Effective date has been formatted and defaulted to:',
+      },
     }),
+
     branch_status: OptionField({
       label: 'Branch Status',
       options: {
         baker: { label: 'BAKER' },
         tradewinds: { label: 'TRADEWINDS' },
-        consolidated: { label: 'Consolidated' }
+        consolidated: { label: 'Consolidated' },
       },
     }),
-    status_effective_date: DateField({
-      label: "Status Effective Date"
+
+    //ifempty = 1900-01-01
+    status_effective_date: SmartDateField({
+      label: 'Status Effective Date',
+      formatString: 'yyyy-MM-dd',
+      default: new Date('1900-01-01'),
+      annotations: {
+        default: true,
+        defaultMessage: 'Effective date has been formatted and defaulted to:',
+      },
     }),
-    status_end_date: DateField({
-      label: "Status End Date"
+
+    //ifempty = 1900-01-01
+    status_end_date: SmartDateField({
+      label: 'Status End Date',
+      formatString: 'yyyy-MM-dd',
+      default: new Date('1900-01-01'),
+      annotations: {
+        default: true,
+        defaultMessage: 'Effective date has been formatted and defaulted to:',
+      },
     }),
     notes: TextField({
-      label: "Notes"
+      label: 'Notes',
     }),
   },
 
-  
   {
     allowCustomFields: false,
     readOnly: false,
@@ -125,11 +145,18 @@ const Branch_Supplemental = new Sheet(
 //   sheet: 'Supplemental3'
 // })
 
-export default new Workbook({
-  name: 'Branch Supplemental',
-  namespace: 'Branch Supplemental',
-  sheets: {
-    Branch_Supplemental,
+//Workbook  - Update to reference your Workbook with Sheet(s)
+export default new SpaceConfig({
+  name: 'Watsco',
+  slug: 'Watscosc',
+  workbookConfigs: {
+    basic: new Workbook({
+      name: 'Watsco',
+      slug: 'WatscoWorkbook',
+      namespace: 'Watsco',
+      sheets: {
+        Branch_Supplemental,
+      },
+    }),
   },
-  // portals: [Supplemental3Portal],
 })
